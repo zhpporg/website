d76705b808319c54033e5a1903e9ba1b7718f324
Cong-Xin Qiu <ozoox.o@gmail.com>
2020-09-12 00:18:29 -0400

    Change baseURL

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ hugo
 cd ..
 git clone git@github.com:zhpporg/zhpporg.github.io.git
 cd zhpporg.github.io
-#git rm -rf .
-#git clean -fxd
+git rm -rf .
+git clean -fxd
 mv ../zhpp/public/* .
 git add -A
 git commit -m "$(date)"
